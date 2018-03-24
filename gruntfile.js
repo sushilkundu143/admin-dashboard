37e3c8000e7d2c7f70de556d191252100911869f
sushilkundu143 <sushilkundu143@gmail.com>
2018-03-24 21:42:22 +0530

    add update in sass

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,25 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     sass: {
-        dist: {
-            src: 'scss/sb-admin-2.scss',
-            dest: 'dist/css/sb-admin-2.css'
-        }
-    }
+      dist: {
+        src: 'scss/sb-admin-2.scss',
+        dest: 'dist/css/sb-admin-2.css'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
   });
+
   grunt.loadNpmTasks('grunt-node-sass');
   grunt.registerTask('default', ['sass']);
+  grunt.loadNpmTasks('grunt-contrib-watch');  
+
 };
