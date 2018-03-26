92500503c1de8ddfe2cc8afdf15086cdf0f2ae46
Sushil Kundu <sushil@goodservice.in>
2018-03-26 11:54:55 +0530

    add update

--- dist/js/menu-toggle.js
+++ dist/js/menu-toggle.js
@@ -9,9 +9,32 @@ $(document).ready(function(){
     });
 
     $("#notification-toggle").click(function(){
-        $('.notification-panel').animate(function(){
-            right: '0px'
-        },100).toggle();
+        $('.notification-panel').animate({display : 'block', right: '0px'}, 500);
     });
 
+    $(".panel-heading a.panel-close").click(function(){
+        $('.notification-panel').animate({display : 'none', right: '-400px'}, 500);
+    });
+
+});
+
+$(document).ready(function () {
+    $('#dataTables-example').DataTable({
+        responsive: true
+    });
+    $('#dataTables-example1').DataTable({
+        responsive: true
+    });
+    $('#dataTables-example2').DataTable({
+        responsive: true
+    });
+});
+// $('.sidebar').height($("body").height());
+$('#calendar-demo').dcalendar();
+
+$(function () {
+    $(".input-group-btn .dropdown-menu li a").click(function () {
+        var selText = $(this).html();
+        $(this).parents('.input-group-btn').find('.btn-search').html(selText);
+    });
 });
